b02d67bd478270a4bebcb280594b6f7748cf1b8a
DevShah0902 <devshah0902@gmail.com>
2025-07-15 21:08:36 -0700

    did the html+css for the project, need to figure out how to do the operate stuff

--- javascript.js
+++ javascript.js
@@ -13,3 +13,35 @@ function multiply(a,b){
 function divide(a,b){
     return a/b
 }
+
+let num1
+let num2
+let Operator
+
+function operate(num1, operator, num2){
+    if (operator = "+"){
+        add(num1,num2)
+    } 
+    else if (operator = "-"){
+        subtract(num1,num2)
+    }
+    else if (operator = "x"){
+        multiply(num1,num2)
+    }
+    else if (operator = "÷"){
+        divide(num1,num2)
+    }
+}
+
+const display = document.querySelector("#display")
+const digits = document.querySelectorAll(".number")
+
+console.log(digits[1].value)
+console.log(digits.length)
+for (let i = 0; i < digits.length; i++){
+    digits[i].addEventListener("click", () => {
+        console.log(digits[1].value)
+        let displayContent = digits[i].value
+        display.textContent += displayContent
+    })
+}
